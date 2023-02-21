3c42dcc9951ad39fbc6e42a6a5aa04502348907b
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-02-21 23:26:18 +0000

    Tue, Feb 21, 2023, 5:26 PM -06:00

--- pset1/calculatorlong.c
+++ pset1/calculatorlong.c
@@ -4,11 +4,12 @@
 int main(void)
 {
     // Prompt user for x
-    int x = get_int("x: ");
+    long x = get_long("x: ");
 
     // Prompt user for y
-    int y = get_int("y: ");
+    long y = get_long("y: ");
 
     // Perform addition
-    printf("%i\n", x + y);
+    float z = (float) x / (float) y;
+    printf("%.5f\n", z);
 }
