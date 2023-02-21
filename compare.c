333389e1ba2395f4dbf5a2e09a034b8d96d3f92e
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-02-21 20:58:24 +0000

    Tue, Feb 21, 2023, 2:58 PM -06:00

--- compare.c
+++ compare.c
@@ -1,2 +1,11 @@
 #include <cs50.h>
-#include 
+#include <stdio.h>
+
+int main(void){
+    int x = get_int("What's x?");
+    int y = get_int("What's y?");
+
+    if(x<y){
+        printf("x is less than y\n");
+    }
+}
