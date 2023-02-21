adad4c81b2862d819724e7dcb43e601e47f8ed7c
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-02-21 22:36:50 +0000

    Tue, Feb 21, 2023, 4:36 PM -06:00

--- pset1/mario2.c
+++ pset1/mario2.c
@@ -3,8 +3,11 @@
 
 int main(void)
 {
-    int n
-    int n = get_int("Size: ");
+    int n;
+    do{
+        n = get_int("Size: ");
+    }
+    while(n<1);
 
     for (int i = 0; i < n; i++)
     {
