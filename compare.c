4c70acac041613f3cb2f1172a8273b0a4bf4fed0
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-02-21 21:01:23 +0000

    Tue, Feb 21, 2023, 3:01 PM -06:00

--- compare.c
+++ compare.c
@@ -8,4 +8,10 @@ int main(void){
     if(x<y){
         printf("x is less than y\n");
     }
+    else if(x>y){
+        printf("x is more than y\n");
+    }
+    else{
+        printf("x is equal to y\n");
+    }
 }
