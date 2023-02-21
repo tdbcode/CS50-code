5911003d1963fe02bd0f6161faed9cec4bc514ea
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-02-21 20:53:14 +0000

    Tue, Feb 21, 2023, 2:53 PM -06:00

--- hello.c
+++ hello.c
@@ -1,6 +1,8 @@
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    printf("Hello, World\n");
+    string answer = get_string("What's your name? ");
+    printf("Hello, %s\n", answer);
 }
