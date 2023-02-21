7272c6f6e56507f57a8aeed86624340437091d77
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-02-21 22:40:25 +0000

    Tue, Feb 21, 2023, 4:40 PM -06:00

--- pset1/mario2.c
+++ pset1/mario2.c
@@ -1,14 +1,27 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int get_size(void);
+void print_guid(n);
+
 int main(void)
 {
+    int n = get_size();
+    print_grid(n);
+}
+
+int get_size(void){
+    // Get size of grid
     int n;
     do{
         n = get_int("Size: ");
     }
     while(n<1);
+    return n;
+}
 
+void print_grid(int n){
+    // Print grid of bricks
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
