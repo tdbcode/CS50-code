31026f65a76b66140b1d26935a65d61a8567cafe
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-02-21 22:33:44 +0000

    Tue, Feb 21, 2023, 4:33 PM -06:00

---
+++ pset1/mario.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        printf("?");
+    }
+    printf("\n");
+
+    for (int j = 0; j < 3; j++)
+    {
+        printf("#\n");
+    }
+}
