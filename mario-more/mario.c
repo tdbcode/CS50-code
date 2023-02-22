609c5b86c05725b9e261f5e18d12e0d5f1950e6d
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-02-22 01:49:35 +0000

    Tue, Feb 21, 2023, 7:49 PM -06:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -3,23 +3,14 @@
 
 int main(void)
 {
-        for (int i = 0; i < 4; i++)
+    int height = get_int("Height: ");
+    int width = height;
+    for (int i = 0; i < height; i++)
     {
-        printf("?");
-    }
-    printf("\n");
-
-    for (int j = 0; j < 3; j++)
-    {
-        printf("#\n");
-    }
-
-   const int n = 3;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
+        for (int j = 2; j > width; j++)
         {
             printf("#");
+            width--;
         }
         printf("\n");
     }
