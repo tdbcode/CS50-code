8e36707f29d0be754b0bb0ab1630bb7f8b567b39
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-02-22 23:18:17 +0000

    Wed, Feb 22, 2023, 5:18 PM -06:00

--- readability/readability.c
+++ readability/readability.c
@@ -5,18 +5,23 @@
 
 int count_letters(string text);
 int count_words(string text);
+int count_sentences(string text);
 
 int main(void)
 {
     string sentence = get_string("Text: ");
     int letters = 0;
     int words = 0;
+    int sentences = 0;
     printf("%s\n", sentence);
 
     letters = count_letters(sentence);
     printf("%i letters\n", letters);
 
     words = count_words(sentence);
+    printf("%i words\n", words);
+
+    sentences = count_sentences(sentence);
     printf("%i words\n", words);
 
 }
@@ -41,7 +46,6 @@ int count_letters(string text)
 
 int count_words(string text)
 {
-    text = text
     char temp;
     int words = 0;
 
@@ -57,4 +61,22 @@ int count_words(string text)
 
     words++;
     return words;
+}
+
+int count_sentences(string text)
+{
+    char temp;
+    int sentences = 0;
+
+    for (int c = 0; c < strlen(text); c++)
+    {
+        temp = text[c];
+
+        if (isblank(temp) && temp != 45)
+        {
+            words++;
+        }
+    }
+
+    return sentences;
 }
