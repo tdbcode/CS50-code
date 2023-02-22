7e23393b92470dead8ada5c68409034f1aa9e8f0
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-02-22 23:29:39 +0000

    Wed, Feb 22, 2023, 5:29 PM -06:00

--- readability/readability.c
+++ readability/readability.c
@@ -9,24 +9,24 @@ int count_sentences(string text);
 
 int main(void)
 {
-    string sentence = get_string("Text: ");
+    string sentence = get_string("Text: ");     // Ask the user for the text passage
     int letters = 0;
     int words = 0;
     int sentences = 0;
     long index = 0;
-    printf("%s\n", sentence);
+    printf("%s\n", sentence);                 // Output the text entered
 
-    letters = count_letters(sentence);
-    printf("%i letters\n", letters);
+    letters = count_letters(sentence);       // Save number of letters using count letters function
+    printf("%i letters\n", letters);        // Output number of letters
 
-    words = count_words(sentence);
-    printf("%i words\n", words);
+    words = count_words(sentence);          // Save number of words using count words function
+    printf("%i words\n", words);            // Output number of words
 
-    sentences = count_sentences(sentence);
-    printf("%i sentences\n", sentences);
+    sentences = count_sentences(sentence); // Save number of sentences using count sentences function
+    printf("%i sentences\n", sentences);   // Output number of words
 
-    index = 0.0588 * (words/100) - 0.296 * (sentences/100) - 15.8;
-    printf("%ld index\n", index);
+    index = 0.0588 * (words / 100) - 0.296 * (sentences / 100) - 15.8; // Perform readability calculation
+    printf("%ld index\n", index);           // Output readability
 
 }
 
@@ -35,11 +35,13 @@ int count_letters(string text)
     char temp;
     int letters = 0;
 
+    //Iterate through each character in the sentence
     for (int c = 0; c < strlen(text); c++)
     {
-        temp = text[c];
+        temp = text[c]; // Set the current character to a temp variable
 
-        if(!(ispunct(temp) || isblank(temp)))
+        // Check if current character is either not a space or a puncuation and add 1 to letters
+        if (!(ispunct(temp) || isblank(temp)))
         {
             letters++;
         }
@@ -53,17 +55,19 @@ int count_words(string text)
     char temp;
     int words = 0;
 
+    //Iterate through each character in the sentence
     for (int c = 0; c < strlen(text); c++)
     {
-        temp = text[c];
+        temp = text[c]; // Set the current character to a temp variable
 
+        // Check if current character is either a space or and not a hypen (-) and add 1 to words
         if (isblank(temp) && temp != 45)
         {
             words++;
         }
     }
 
-    words++;
+    words++; // Add one word to include end of sentence (which isn't counter otherwise)
     return words;
 }
 
@@ -72,10 +76,12 @@ int count_sentences(string text)
     char temp;
     int sentences = 0;
 
+    //Iterate through each character in the sentence
     for (int c = 0; c < strlen(text); c++)
     {
-        temp = text[c];
+        temp = text[c]; // Set the current character to a temp variable
 
+        // Check if current character is either an explanation mark, aperiod or a question mark and add 1 to sentences
         if (temp == 21 || temp == 46 || temp == 63)
         {
             sentences++;
