502f81e072f19a873d13325fdb792f4b942ce6cc
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-02-22 01:44:34 +0000

    Tue, Feb 21, 2023, 7:44 PM -06:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,26 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+        for (int i = 0; i < 4; i++)
+    {
+        printf("?");
+    }
+    printf("\n");
+
+    for (int j = 0; j < 3; j++)
+    {
+        printf("#\n");
+    }
+
+   const int n = 3;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
