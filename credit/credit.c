bd3fc40b21cd6b859155d9b0e8673b70e1669b5b
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-02-22 03:52:20 +0000

    Tue, Feb 21, 2023, 9:52 PM -06:00

--- credit/credit.c
+++ credit/credit.c
@@ -4,6 +4,18 @@
 int main(void)
 {
     long creditno = get_long("Enter the credit card number: ");
+    long creditno2 = creditno;
+    int counter = 0;
 
-    for(int i = creditno.length())
+    while (creditno2 > 0)
+    {
+        creditno2 = creditno2/10;
+        counter++;
+    }
+
+    for(int i = creditno2; i > 0; i=i-2)
+    {
+        
+    }
+
 }
