b75dcbc19962a7c6e867dc31d277daf8a0d496c7
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-02-22 23:01:58 +0000

    Wed, Feb 22, 2023, 5:01 PM -06:00

--- readability/readability.c
+++ readability/readability.c
@@ -3,9 +3,23 @@
 #include <string.h>
 #include <ctype.h>
 
+int count_letters(string text);
+
 int main(void)
 {
     string sentence = get_string("Text: ");
+    int letters = 0;
+    printf("%s\n", sentence);
 
-    for 
+    letters = count_letters(sentence);
+    printf("%i letters\n", letters);
+
+}
+
+int count_letters(string text)
+{
+    int letters = 0;
+
+    return letters;
+
 }
