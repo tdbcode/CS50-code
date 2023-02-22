7f597ba82fbebbc8343a926ccddd76c265d7e67b
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-02-22 04:08:06 +0000

    Tue, Feb 21, 2023, 10:08 PM -06:00

--- credit/credit.c
+++ credit/credit.c
@@ -6,23 +6,24 @@ int main(void)
     long creditno = 0;
     long creditno2 = 0;
     int counter = 0;
+    int currentNo = 0;
 
-    do
-    {
+   // do
+    //{
         creditno = get_long("Enter the credit card number: ");
         creditno2 = creditno;
 
         while (creditno2 > 0)
         {
             creditno2 = creditno2/10;
-            counter++;
+            currentNo = creditno2 % 10;
+            creditno2 = creditno2/10;
+            counter=counter+2;
+            printf("%i\n", currentNo);
         }
-    } while ((counter > 16) && (counter < 13) && (counter == 14));
+ //   } while ((counter > 16) && (counter < 13) && (counter == 14));
 
-    printf("%i\n", counter);
+   // printf("%i\n", counter);
 
-   // while(counter>16)
-
-   // int creditNos[counter];
 
 }
