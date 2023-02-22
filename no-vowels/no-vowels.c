d056fcdada1a450de374f52dc31fccfc734c11b9
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-02-22 21:13:53 +0000

    Wed, Feb 22, 2023, 3:13 PM -06:00

--- no-vowels/no-vowels.c
+++ no-vowels/no-vowels.c
@@ -5,8 +5,27 @@
 
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+
+void replace(string argv[]);
 
 int main(int argc, string argv[])
 {
-    
+    if (argc != 2)
+    {
+        printf("Missing command-line argument\n");
+        return 1;
+    }
+
+    replace(argv);
+
+    return 0;
+
+
+}
+
+void replace(string argv[]){
+
+    for(int c = 0; c < )
+    printf("hello, %c\n", argv[1][1]);
 }
