8ffbdfde110fa398c5c0ccab56b50e73a016f961
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-02-23 01:36:08 +0000

    Wed, Feb 22, 2023, 7:36 PM -06:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -85,7 +85,10 @@ int main(int argc, string argv[])
         int status[wordsize];
 
         // set all elements of status array initially to 0, aka WRONG
-        // TODO #4
+        for (int x = 0; x < wordsize; x++)
+        {
+            status[x] = 0;
+        }
 
         // Calculate score for the guess
         int score = check_word(guess, wordsize, status, choice);
