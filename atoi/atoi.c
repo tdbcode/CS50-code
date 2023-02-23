3361a16f3921c533f6dc986f870a0db0ba455085
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-02-23 04:46:43 +0000

    Wed, Feb 22, 2023, 10:46 PM -06:00

--- atoi/atoi.c
+++ atoi/atoi.c
@@ -27,16 +27,18 @@ int convert(string input)
 {
     int value = 0;
     int length = strlen(input);
-    strei currentLetter[length-1];
+    char currentLetter[length-1];
 
     value = input[length - 1];
 
     for (int c = 0; c < length-1; c++)
     {
-        currentLetter += input[c];
+        currentLetter[c] = input[c];
     }
 
-    printf("%s\n", currentLetter);
-    return 0;
-    //return convert(input);
+    strncpy(input, currentLetter, length-1);
+    input[length-1] = '\0';
+
+    printf("%s\n", input);
+    return convert(input);
 }
