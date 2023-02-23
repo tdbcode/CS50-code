2816a3a81a0a42996eb8d10c04818fbfed0c961b
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-02-23 22:47:23 +0000

    Thu, Feb 23, 2023, 4:47 PM -06:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -116,7 +116,16 @@ bool vote(int rank, string name, int ranks[])
 // Update preferences given one voter's ranks
 void record_preferences(int ranks[])
 {
-    for (int i = 0; i < candidate_count; i++)
+    int i = 0; // Tom
+    int j = 1; // Barry
+    int k = 2; // Jill
+
+    if (ranks[0])
+    {
+        preferences[0][j] += 1;
+    }
+
+   /* for (int i = 0; i < candidate_count; i++)
     {
         for (int j = 0; j < candidate_count; j++)
         {
@@ -131,7 +140,7 @@ void record_preferences(int ranks[])
             }
         }
 
-    }
+    }*/
 
 }
 
