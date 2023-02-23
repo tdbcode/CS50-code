a2538a7af534689853a4d39fb1bea1b414880647
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-02-23 00:27:10 +0000

    Wed, Feb 22, 2023, 6:27 PM -06:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -27,7 +27,7 @@ int main(int argc, string argv[])
 {
     // ensure proper usage
     // TODO #1
-
+    
     int wordsize = 0;
 
     // ensure argv[1] is either 5, 6, 7, or 8 and store that value in wordsize instead
