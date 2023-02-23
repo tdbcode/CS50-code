b671f00dd8da9107bfd6e34a40bb7cd9571ce90b
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-02-23 18:55:23 +0000

    Thu, Feb 23, 2023, 12:55 PM -06:00

--- snackbar/snackbar.c
+++ snackbar/snackbar.c
@@ -74,6 +74,27 @@ int main(void)
 // Add at least the first for items to the menu array
 void add_items(void)
 {
+    menu[0].item = "Burger";
+    menu[0].price = 9.5;
+    menu[1].item = "Vegan Burger";
+    menu[1].price = 11;
+    menu[2].item = "Hot Dog";
+    menu[2].price = 5;
+    menu[3].item = "Cheese Dog";
+    menu[3].price = 7;
+    menu[4].item = "Fries";
+    menu[4].price = 5;
+    menu[5].item = "Cheese Fries";
+    menu[5].price = 6;
+    menu[6].item = "Cold Pressed Juice";
+    menu[6].price = 7;
+    menu[7].item = "Cold Brew";
+    menu[7].price = 3;
+    menu[8].item = "Water";
+    menu[8].price = 2;
+    menu[9].item = "Soda";
+    menu[9].price = 2;
+
     return;
 }
 
