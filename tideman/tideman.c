e74580e0f2600c5cbb091416ebde2a9a8baf6e20
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-02-23 23:42:47 +0000

    Thu, Feb 23, 2023, 5:42 PM -06:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -138,12 +138,19 @@ void add_pairs(void)
         // Iterate through losers in preferences
         for (int l = 1; l < candidate_count; l++)
         {
-            if (preferences[w][l] != preferences[l][w])
+            // Note to self: There can't be a [0][0] because it's the same candidate
+            // If [winner][loser], e.g. [0][1] is more than [loser][winner], e.g. 1, 0 then
+            if (preferences[w][l] > preferences[l][w])
             {
                 pairs[w].winner = w;
-                pairs[w].loser = l;
-                printf("Candidate : %i vs %i : \n", pairs[w].winner, pairs[l].loser);
+                pairs[l].loser = l;
             }
+            else if (preferences[w][l] < preferences[l][w])
+            {
+                pairs[w].winner = l;
+                pairs[l].loser = w;
+            }
+            printf("Candidate : %i vs %i : \n", pairs[w].winner, pairs[l].loser);
         }
     }
 
