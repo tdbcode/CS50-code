2d4a75b456582590328910ee14c3fbf63864b21b
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-02-24 21:02:40 +0000

    Fri, Feb 24, 2023, 3:02 PM -06:00

--- addresses.c
+++ addresses.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
     int n = 50;
     int *p = &n; //Gets memory location (pointer) of n &n = memory location of, *p = pointer
     printf("%i\n", *p); //Notice that the printf line prints the integer at the location of p
+
+    string s = "HI!";
+    printf("%p\n", s);
+    printf("%p\n", &s[0]);
+    printf("%p\n", &s[1]);
+    printf("%p\n", &s[2]);
+    printf("%p\n", &s[3]);
+
+    char *c = "HI!";
+    printf("%s\n", c);
 }
