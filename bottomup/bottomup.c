0e58c407d0bc0f096baafc8093f182a9a8e5fe6b
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-02-26 19:05:45 +0000

    Sun, Feb 26, 2023, 1:05 PM -06:00

--- bottomup/bottomup.c
+++ bottomup/bottomup.c
@@ -52,7 +52,7 @@ int main(int argc, char *argv[])
         printf("Unsupported file format.\n");
         return 4;
     }
-    
+
     // Write outfile's BITMAPFILEHEADER
     fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
 
@@ -63,10 +63,10 @@ int main(int argc, char *argv[])
     int padding = (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
 
     // Iterate over infile's scanlines
-    for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++)
+    for (int i = abs(bi.biHeight); i > 0; i--)
     {
         // Iterate over pixels in scanline
-        for (int j = 0; j < bi.biWidth; j++)
+        for (int j = bi.biWidth; j > 0 ; j--)
         {
             // Temporary storage
             RGBTRIPLE triple;
