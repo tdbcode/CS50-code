973b964843804e0752de525782f0ddace8e681de
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-02-26 20:33:48 +0000

    Sun, Feb 26, 2023, 2:33 PM -06:00

--- license/license.c
+++ license/license.c
@@ -21,26 +21,18 @@ int main(int argc, char *argv[])
 
     FILE *infile = fopen(argv[1], "r");
 
-    // Allocate the memory first
-    for (int p = 0; p < 8; p++)
-    {
-        plates[p] = malloc(sizeof(buffer));
-        plates[p] = fread(buffer, 1, 7, infile);
-    }
-
-
-
     int idx = 0;
 
-/*    while (fread(buffer, 1, 7, infile) == 7)
+    while (fread(buffer, 1, 7, infile) == 7)
     {
         // Replace '\n' with '\0'
         buffer[6] = '\0';
 
         // Save plate number in array
-        plates[idx] = buffer;
+        plates[idx] = malloc(sizeof(buffer));
+        plates[idx] = &buffer;
         idx++;
-    }*/
+    }
 
     for (int i = 0; i < 8; i++)
     {
