ad831e93443bf967edb1103ed214a59b562809b3
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-02-26 19:23:05 +0000

    Sun, Feb 26, 2023, 1:23 PM -06:00

--- bottomup/bottomup.c
+++ bottomup/bottomup.c
@@ -53,6 +53,7 @@ int main(int argc, char *argv[])
         return 4;
     }
 
+    bi.biHeight = bi.biHeight * - 1;
     // Write outfile's BITMAPFILEHEADER
     fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
 
