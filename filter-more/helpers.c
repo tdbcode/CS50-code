40b3e7b5fbd0a1ad1423f564bf239314f52dc0f0
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-03 21:13:06 +0000

    Fri, Mar 3, 2023, 3:13 PM -06:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -3,7 +3,17 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+    for (int h = 0; h < height; h++)
+    {
+        for (int w = 0; h < width; w++)
+        {
+            RGBTRIPLE currentTriple = image[h][w];
+            if (currentTriple.rgbtRed < 255 & currentTriple.rgbtBlue < 255 & currentTriple.rgbtGreen < 255)
+            {
+                
+            }
+        }
+    }
     return;
 }
 
