eb875cb9ecb857dc6b2626871d6c83dc8f06f375
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-03 21:59:43 +0000

    Fri, Mar 3, 2023, 3:59 PM -06:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -3,14 +3,14 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    float average = 0;
+    double average = 0;
     for (int h = 0; h < height; h++)
     {
         for (int w = 0; w < width; w++)
         {
             if (image[h][w].rgbtRed < 255 || image[h][w].rgbtBlue < 255 || image[h][w].rgbtGreen < 255)
             {
-                average = image[h][w].rgbtRed / 3 + image[h][w].rgbtGreen / 3 +  image[h][w].rgbtBlue / 3;
+                average = (image[h][w].rgbtRed + image[h][w].rgbtGreen +  image[h][w].rgbtBlue) / 3;
                 image[h][w].rgbtRed = average;
                 image[h][w].rgbtBlue = average;
                 image[h][w].rgbtGreen = average;
