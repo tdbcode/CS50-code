51b91aec3339d0fd1927fc300ed5b130977d99b1
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-06 15:46:39 +0000

    Mon, Mar 6, 2023, 9:46 AM -06:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -68,35 +68,35 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int w1 = 0; w1 < width; w1++) // get current pixel in temp array
         {
-            // Now need to check up and down but only if not edge pixels
-            if (h1 > 0 && h1 < height - 1 && w1 > 0 && w1 < width - 1)
+            // this does up
+            for(int i = -1; i < 2; i++)
             {
-                // this does up
-                for(int i = -1; i < 2; i++)
+                // this does across
+                for (int j = -1; j < 2; j++)
                 {
-                    // this does across
-                    for (int j = -1; j < 2; j++)
+                    // Now need to check up and down but only if not edge pixels
+                    if ((h1 + i <= 0 || h1 + i >= height) && (w1 + j <= 0 && w1 + j >= width))
                     {
-                        // need to offset each pixel for calculation in array - so current pixel (height-1 width-1, height-1 width, height -1 width+1)
-                        // (then height width -1, height width, height width +1, then height + 1 width - 1, height + 1 width, height +1 width +1)
-                        rgbtRedTemp = image[h1+i][w1+j].rgbtRed; // get sum of all red
-                        rgbtBlueTemp = image[h1+i][w1+j].rgbtBlue; // get sum of all blue
-                        rgbtGreenTemp = image[h1+i][w1+j].rgbtGreen; // get sum of all green
+                    // need to offset each pixel for calculation in array - so current pixel (height-1 width-1, height-1 width, height -1 width+1)
+                    // (then height width -1, height width, height width +1, then height + 1 width - 1, height + 1 width, height +1 width +1)
+                    rgbtRedTemp += image[h1+i][w1+j].rgbtRed; // get sum of all red
+                    rgbtBlueTemp += image[h1+i][w1+j].rgbtBlue; // get sum of all blue
+                    rgbtGreenTemp += image[h1+i][w1+j].rgbtGreen; // get sum of all green
                     }
                 }
-                temp[h1][w1].rgbtRed = round(rgbtRedTemp / 9); // do average of all 9 pixels for red
-                temp[h1][w1].rgbtGreen = round(rgbtGreenTemp / 9);
-                temp[h1][w1].rgbtBlue = round(rgbtBlueTemp / 9);
             }
+            temp[h1][w1].rgbtRed = round(rgbtRedTemp / 9); // do average of all 9 pixels for red
+            temp[h1][w1].rgbtGreen = round(rgbtGreenTemp / 9);
+            temp[h1][w1].rgbtBlue = round(rgbtBlueTemp / 9);
         }
     }
 
-        for (int h = 0; h < height; h++) // copy array to temp array
+    for (int h = 0; h < height; h++) // copy temp array back to main image array
+    {
+        for (int w = 0; w < width; w++)
         {
-            for (int w = 0; w < width; w++)
-            {
-                image[h][w] = temp[h][w];
-            }
+            image[h][w] = temp[h][w];
+        }
     }
     return;
 }
