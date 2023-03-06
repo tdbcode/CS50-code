09b47b8684d629449b36da6fd4ff3e1092ef82af
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-06 18:00:51 +0000

    Mon, Mar 6, 2023, 12:00 PM -06:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -123,7 +123,7 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
     float  rgbtGreenTemp = 0;
     float  rgbtRedTemp = 0;
     int counter = 0;
-    int muliplierArray = {-1, 0, 1}{-2, 0, 2}{-1, 0, 1};
+    int muliplierArray[3][3] = {{-1, 0, 1}{-2, 0, 2}{-1, 0, 1}};
 
     for (int h = 0; h < height; h++) // copy array to temp array
     {
