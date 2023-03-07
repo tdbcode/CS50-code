bbe6c1e22f79ba88dfbfa9d3697874caa642f02a
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-07 15:09:27 +0000

    Tue, Mar 7, 2023, 9:09 AM -06:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -28,9 +28,9 @@ int main(int argc, char *argv[])
     }
 
     // Read header
-    int headersize = sizeof(WAVHEADER);
+    int headersize = sizeof(WAVHEADER); // Get header size
     WAVHEADER header;
-    fread(&header, headersize, 1, input);
+    fread(&header, headersize, 1, input); // Load header from input file using header size
 
     // Use check_format to ensure WAV format
     check_format(header);
@@ -44,30 +44,31 @@ int main(int argc, char *argv[])
     }
 
     // Write header to file
-    fwrite(&header, headersize, 1, output);
+    fwrite(&header, headersize, 1, output);  // Write header to output file using header size
 
     // Use get_block_size to calculate size of block
     int blocksize = get_block_size(header);
 
     // Write reversed audio to file
+    BYTE currentSample[blocksize];  // Temp sample for reading and writing limited to block size of each sample
+    fseek(input, blocksize, SEEK_END); // Go to end of file + blocksize (4) so that when the loop is run it works back by 2
+    // int currentLocator = ftell(input); // for testing purposes (was used before not unneeded in the end)
+    // printf("Blocksize: %u\n", blocksize); // for testing purposes
+    // printf("First Sample: %i\n", headersize); // for testing purposes
+    // printf("Current Locator: %i\n", currentLocator); // for testing purposes
+    //printf("File Size: %i\n", filesize); // for testing purposes
 
-    BYTE currentSample[blocksize];
-    fseek(input, blocksize, SEEK_END);
-    int currentLocator = ftell(input);
-    printf("Blocksize: %u\n", blocksize);
-    printf("First Sample: %i\n", headersize);
-    printf("Current Locator: %i\n", currentLocator);
-    //printf("File Size: %i\n", filesize);
-
+    // While the location in the file minus block size is more than header size continue to run the algorithm
     while (ftell(input) - blocksize > headersize)
     {
-        fseek(input, - 2 * blocksize, SEEK_CUR);
-        fread(&currentSample, blocksize, 1, input);
-        fwrite(&currentSample, blocksize, 1, output);
-        currentLocator -= blocksize;
+        fseek(input, - 2 * blocksize, SEEK_CUR); // Take current location and move it back 2 blocks
+        fread(&currentSample, blocksize, 1, input); // Read in the current block from the input file
+        fwrite(&currentSample, blocksize, 1, output); // Write block to output file.
+        // currentLocator -= blocksize; // for testing purposes / prior attempt
     }
-    printf("Ftell: %ld\n", ftell(input));
+    // printf("Ftell: %ld\n", ftell(input)); // for testing purposes
 
+    // close streams
     fclose(input);
     fclose(output);
     return 0;
