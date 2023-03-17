05112335bde595f722b9cde0ae97dafd3c72bda1
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-17 16:18:44 +0000

    Fri, Mar 17, 2023, 11:18 AM -05:00

--- linkedlist.c
+++ linkedlist.c
@@ -11,6 +11,16 @@ node;
 
 int main(int argc, char *argv[])
 {
-    node *head = null; // Head of linked list has a null pointer at the start
+    node *head = NULL; // Head of linked list has a null pointer at the start
+
+    int number = 0;
     
+    for
+    // Allocate node for inputted number
+    node *n = malloc(sizeof(node));
+    if (n == NULL)
+    {
+        printf("No value inputted.");
+        return 1;
+    }
 }
