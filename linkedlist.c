5d7fdd7f250a820f963f9e1618074b9991058ccd
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-17 16:14:23 +0000

    Fri, Mar 17, 2023, 11:14 AM -05:00

--- linkedlist.c
+++ linkedlist.c
@@ -7,4 +7,10 @@ typedef struct node
     int number;
     struct node *next; // Memory address for next node
 }
+node;
 
+int main(int argc, char *argv[])
+{
+    node *head = null; // Head of linked list has a null pointer at the start
+    
+}
