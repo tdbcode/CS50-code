36973df5d5097688819c69294ea70236289d9be3
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-20 19:33:41 +0000

    Mon, Mar 20, 2023, 2:33 PM -05:00

--- hashtable.c
+++ hashtable.c
@@ -54,7 +54,6 @@ void addPhrase(node *newnode, int index)
     if (table[index] == NULL)
     {
         table[index] = newnode;
-        free(newnode);
         //printf("Phrase added \n");
         return;
     }
