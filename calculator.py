0170b734ec8a3cfd769de6a7bb2616290b98e6da
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-21 23:31:45 +0000

    Tue, Mar 21, 2023, 6:31 PM -05:00

--- calculator.py
+++ calculator.py
@@ -6,3 +6,13 @@ y = int(input("y: "))
 
 # Perform addition
 print(x + y)
+
+# Prompt user for x
+x = int(input("x: "))
+
+# Prompt user for y
+y = int(input("y: "))
+
+# Divide x by y
+z = x / y
+print(f"{z:.2f}")
