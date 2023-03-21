a6057d3d63929ba2af7a60df3388636f2f3b46e1
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-21 13:43:45 +0000

    Tue, Mar 21, 2023, 8:43 AM -05:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -41,6 +41,10 @@ person *create_family(int generations)
 {
     // TODO: Allocate memory for new person
     person *tempperson = malloc(sizeof(person));
+    if (tempperson = NULL)
+    {
+        return 1;
+    }
 
     // If there are still generations left to create
     if (generations > 1)
@@ -50,6 +54,7 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
+
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
 
