a5078d09f9f83925931582e420b10f9c6b81b8a7
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-22 15:38:20 +0000

    Wed, Mar 22, 2023, 10:38 AM -05:00

--- Python/bank/bank.py
+++ Python/bank/bank.py
@@ -1,11 +1,11 @@
-import re
+import re # Import regular expression library
 
 greeting = str(input("What was the greeting?"))
 
 words = greeting.split()
 first = words[0]
 # use re.sub|() function
-first = re.sub(",", "", first)
+first = re.sub(",", "", first) # remove commas from string
 
 if (first.lower() == 'hello'):
     amount = 0
