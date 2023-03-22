200827112e0f28280e356c4388daed699814ea2c
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-22 15:58:27 +0000

    Wed, Mar 22, 2023, 10:58 AM -05:00

--- Python/figlet.py
+++ Python/figlet.py
@@ -5,12 +5,16 @@ random.seed()
 figlet = Figlet()
 
 if len(argv) == 2:
+    if (argv[1] != '-f' or argv[1] != '-font'):
+        print("Enter -f or -font followed by font name")
+        sys.exit(1)
     figlet.setFont(font=argv[3])
-elif len(argv) == 1:
-    rand = random.randint(1,20)
-    figlet.setFont(font=rand)
 else:
-    print("Enter -f -fontnamehere")
+    rand = random.randint(1,3)
+    if (rand==1):
+        
+    figlet.setFont(font=rand)
+
 
 s = input("Enter text:" )
 print(figlet.renderText(s))
