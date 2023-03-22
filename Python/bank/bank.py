d7b84cf72389b11594500d180073c320b0875e9d
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-22 14:55:57 +0000

    Wed, Mar 22, 2023, 9:55 AM -05:00

--- Python/bank/bank.py
+++ Python/bank/bank.py
@@ -1,2 +1,13 @@
 greeting = input("What was the greeting?")
 
+words = greeting.split()
+first = words[0]
+
+if first.lower == "hello":
+    amount = 0
+elif first[0].lower == 'h':
+    amount = 20
+else:
+    amount = 100
+
+print(amount)
