375fd78739227f7f558e1e878244971ef7006b68
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-22 16:01:44 +0000

    Wed, Mar 22, 2023, 11:01 AM -05:00

--- Python/figlet.py
+++ Python/figlet.py
@@ -3,17 +3,16 @@ from pyfiglet import Figlet
 import random
 random.seed()
 figlet = Figlet()
+fonts = figlet.getFonts()
 
 if len(argv) == 2:
-    if (argv[1] != '-f' or argv[1] != '-font'):
+    if (argv[1] != "-f" or argv[1] != "-font"):
         print("Enter -f or -font followed by font name")
         sys.exit(1)
     figlet.setFont(font=argv[3])
 else:
-    rand = random.randint(1,3)
-    if (rand==1):
-        
-    figlet.setFont(font=rand)
+    rand = random.randint(1,len(fonts)) - 1
+    figlet.setFont(font=fonts[rand])
 
 
 s = input("Enter text:" )
