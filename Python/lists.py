dead171a12da1c7ec1fbc949f6bee61b3ba5d845
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-22 14:34:20 +0000

    Wed, Mar 22, 2023, 9:34 AM -05:00

--- Python/lists.py
+++ Python/lists.py
@@ -1,13 +1,11 @@
 # Averages three numbers using a list and a loop
 
-from cs50 import get_int
-
 # Get scores
 scores = []
 for i in range(3):
-    score = get_int("Score: ")
+    score = int(input("Score: "))
     scores.append(score)
 
 # Print average
 average = sum(scores) / len(scores)
-print(f"Average: {average}")
+print(f"Average: {average:.2f}")
