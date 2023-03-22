880b3b7f733aeb22882186ee1af0b479bf0390ed
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-22 14:38:27 +0000

    Wed, Mar 22, 2023, 9:38 AM -05:00

---
+++ arguments.py
@@ -0,0 +1,17 @@
+# Prints a command-line argument
+
+from sys import argv
+
+if len(argv) == 2:
+    print(f"hello, {argv[1]}")
+else:
+    print("hello, world")
+
+# Printing command-line arguments, indexing into argv
+
+from sys import argv
+
+for i in range(len(argv)):
+    print(argv[i])
+
+    
