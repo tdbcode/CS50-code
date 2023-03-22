29e15f05c6d5fa414ab6fb39722abdf6d0530cc6
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-22 15:37:03 +0000

    Wed, Mar 22, 2023, 10:37 AM -05:00

--- Python/bank/bank.py
+++ Python/bank/bank.py
@@ -1,7 +1,11 @@
+import re
+
 greeting = str(input("What was the greeting?"))
 
 words = greeting.split()
 first = words[0]
+# use re.sub|() function
+first = re.sub(",", "", first)
 
 if (first.lower() == 'hello'):
     amount = 0
