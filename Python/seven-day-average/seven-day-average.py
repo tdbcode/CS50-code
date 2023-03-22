70ab54e38adb982d1a778ca61df71bc9ef4957d9
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-22 17:19:03 +0000

    Wed, Mar 22, 2023, 12:19 PM -05:00

--- Python/seven-day-average/seven-day-average.py
+++ Python/seven-day-average/seven-day-average.py
@@ -34,7 +34,7 @@ def main():
 
 # TODO: Create a dictionary to store 14 most recent days of new cases by state
 def calculate(reader):
-    data = 
+    data = {}
     for case in reader:
         print(case)
 
