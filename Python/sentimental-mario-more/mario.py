b91f56c9a1ddc25f50b2b86034d30c7dafe0e503
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-23 18:21:03 +0000

    Thu, Mar 23, 2023, 1:21 PM -05:00

--- Python/sentimental-mario-more/mario.py
+++ Python/sentimental-mario-more/mario.py
@@ -1,1 +1,37 @@
-# TODO
+bricks = 0
+height = 0
+
+while (height < 1 or height > 8):
+    height = int(input(Height:))
+
+    for x in range(height):
+        bricks += 1
+
+def spaces(spaces, bricks):
+    print(" ", end="")
+
+    for (int i = 0; i < height; i++)
+    {
+        bricks++;
+
+        spaces(height, bricks);
+
+        brick(bricks);
+
+        printf("  ");
+
+        brick(bricks);
+
+        printf("\n");
+    }
+
+}
+
+void brick(int bricks)
+{
+    for (int b = 0; b < bricks; b++)
+    {
+        printf("#");
+    }
+}
+
