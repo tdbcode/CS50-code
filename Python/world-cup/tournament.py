3784b640f227457b0c150aa2976e306a90b0685f
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-23 16:08:37 +0000

    Thu, Mar 23, 2023, 11:08 AM -05:00

--- Python/world-cup/tournament.py
+++ Python/world-cup/tournament.py
@@ -45,31 +45,36 @@ def main():
         print(f"{team}: {counts[team] * 100 / N:.1f}% chance of winning")
 
 
+# comments for tracing how it works to solve simulate_tournaments
 def simulate_game(team1, team2):
     """Simulate a game. Return True if team1 wins, False otherwise."""
-    rating1 = team1["rating"]
-    rating2 = team2["rating"]
-    probability = 1 / (1 + 10 ** ((rating2 - rating1) / 600))
-    return random.random() < probability
+    rating1 = team1["rating"]  # gets passed in team1 rating
+    rating2 = team2["rating"]  # gets passed in team2 rating
+    probability = 1 / (1 + 10 ** ((rating2 - rating1) / 600))  #calculates probability of winning
+    return random.random() < probability  # returns which team wins
 
 
+# comments for tracing how it works to solve simulate_tournaments
 def simulate_round(teams):
     """Simulate a round. Return a list of winning teams."""
     winners = []
 
     # Simulate games for all pairs of teams
     for i in range(0, len(teams), 2):
-        if simulate_game(teams[i], teams[i + 1]):
-            winners.append(teams[i])
+        if simulate_game(teams[i], teams[i + 1]): ## for each adjacent team in list, call simulate game to get winner
+            winners.append(teams[i])  # if returns true, add team 1 to winners array
         else:
-            winners.append(teams[i + 1])
+            winners.append(teams[i + 1])  # if returns false, add team 2 to winners array
 
     return winners
 
 
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
-    # TODO
+    length = len(teams)  # get length of teams array
+    while length > 1 and teams % 2 == 0):  # while there is more than 1 team in the passed in list
+        
+
 
 
 if __name__ == "__main__":
