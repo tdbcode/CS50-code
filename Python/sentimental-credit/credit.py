10308892214a53bf18934e27bcaf475a1a187f84
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-23 20:33:45 +0000

    Thu, Mar 23, 2023, 3:33 PM -05:00

--- Python/sentimental-credit/credit.py
+++ Python/sentimental-credit/credit.py
@@ -34,7 +34,7 @@ for c in reversed(range(0,clength)):  # run loop in reverse to go from back of c
 
 for a in reversed(range(1,clength,-2)):  # run loop in reverse skipping every other number
     currentNo = ccno[a] * 2  # double the current number in the array
-    while (currentNo > 9):  # check if current number as more than 1 digit
+    if (currentNo > 9):  # check if current number as more than 1 digit
         product = (currentNo % 10) + (currentNo / 10)  # if so then add them together
         currentNo = product  # set new number to current number
 
