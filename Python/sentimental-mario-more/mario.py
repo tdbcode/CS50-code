b431e5b5f6593148f9a052cce2ba0137d1592242
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-23 18:44:31 +0000

    Thu, Mar 23, 2023, 1:44 PM -05:00

--- Python/sentimental-mario-more/mario.py
+++ Python/sentimental-mario-more/mario.py
@@ -1,11 +1,12 @@
 import sys
 
-def spaces(spaces, bricks):
-    for i in reversed(range(bricks, spaces)):
+
+def spaces(spaces, bricks):  # create procedure that passes in spaces and bricks
+    for i in reversed(range(bricks, spaces)):  # run a for loop in reverse that outputs spaces to be run either side of bricks
         print(" ", end="")
 
 
-def brick(bricks):
+def brick(bricks):  # create procedure that outputs bricks based on current depth
     for i in range(bricks):
         print("#", end="")
 
@@ -13,17 +14,18 @@ def brick(bricks):
 bricks = 0
 height = 0
 
-
+# validation to ensure height is not less than 1 or more than 8
 while (height < 1 or height > 8):
     try:
         height = int(input("Height:"))
-    except ValueError:
+    except ValueError:  # when integer is not entered tell used to enter number
         print("You must enter a number")
 
+# for the height entered
 for x in range(height):
-    bricks += 1
-    spaces(height, bricks)
-    brick(bricks)
-    print("  ", end="")
-    brick(bricks)
-    print("")
+    bricks += 1  # add 1 to number of bricks
+    spaces(height, bricks)  # draw spaces
+    brick(bricks)  # draw bricks
+    print("  ", end="")  # draw middle space
+    brick(bricks)  # draw other side of bricks
+    print("")  # make a new line
