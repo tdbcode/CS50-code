2bd445e032fba8f043f2c32a1b24149013c0d7de
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-24 00:25:05 +0000

    Thu, Mar 23, 2023, 7:25 PM -05:00

--- Python/sentimental-credit/credit.py
+++ Python/sentimental-credit/credit.py
@@ -28,18 +28,20 @@ for c in reversed(range(0,clength)):  # run loop in reverse to go from back of c
     ccno[c] = int(creditno2[c])  # take the end number to add to new array
     #creditno2 = creditno2 / 10  # remove last number
     #ccno[c] = int(currentNo)  # add last number back in it's possition in the array
-print(ccno)
-print(len(ccno))
 
-for a in ccno), 0 -2)):  # run loop in reverse skipping every other number
-    print(a)
-    currentNo = ccno[a] * 2  # double the current number in the array
+locator = clength - 2
+
+for a in range(0, clength, 2):  # run loop in reverse skipping every other number
+    currentNo = ccno[locator] * 2  # double the current number in the array
+    print(locator)
     print(currentNo)
     while (currentNo > 9):  # check if current number as more than 1 digit
         product = int(currentNo % 10) + int(currentNo / 10)  # if so then add them together
         currentNo = product  # set new number to current number
     checksum += currentNo
-    checksum += int(ccno[a-1])
+    checksum += int(ccno[locator + 1])
+    print(locator+1)
+    locator -= 2
 print("Checksum:", checksum)
 
 #checksum = sum(twotimed) + sum(regular)  # add current number to checksum
