e731b14e7ec2e17413b4f19a8339327e96027411
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-24 01:32:00 +0000

    Thu, Mar 23, 2023, 8:32 PM -05:00

--- Python/sentimental-readability/readability.py
+++ Python/sentimental-readability/readability.py
@@ -9,25 +9,39 @@ def count_letters(text):
         if isalnum(temp):
             letters += 1;
 
-    return letters;
+    return letters
 
 
 def count_words(text):
-    words = 0;
+    words = 0
 
     # Iterate through each character in the sentence
     for c in range(len(text)):
         temp = text[c]  # Set the current character to a temp variable
 
         # Check if current character is either a space or and not a hypen (-) and add 1 to words
-        if temp.isspace() && temp != 45)
+        if temp.isspace() and temp != "-":
+            words += 1
+
+    words += 1  # Add one word to include end of sentence (which isn't counter otherwise)
+    return words
+
+
+def count_sentences(text):
+    sentences = 0
+
+    # Iterate through each character in the sentence
+    for c in range(len(text)):
+        temp = text[c]  # Set the current character to a temp variable
+
+        # Check if current character is either an explanation mark, aperiod or a question mark and add 1 to sentences
+        if (temp == 33 || temp == 46 || temp == 63)
         {
-            words++;
+            sentences++;
         }
     }
 
-    words++; // Add one word to include end of sentence (which isn't counter otherwise)
-    return words;
+    return sentences;
 }
 
 
@@ -41,8 +55,7 @@ grade = 0
 
 letters = count_letters(sentence)  # Save number of letters using count letters function
 words = count_words(sentence)  # Save number of words using count words function
-
-    sentences = count_sentences(sentence); // Save number of sentences using count sentences function
+sentences = count_sentences(sentence)  # Save number of sentences using count sentences function
 
     index = 0.0588 * letters / words * 100 - 0.296 * sentences / words * 100 - 15.8; // Perform readability calculation
 
@@ -62,26 +75,4 @@ words = count_words(sentence)  # Save number of words using count words function
         printf("Grade %i\n", grade);
     }
 
-}
-
-
-
-int count_sentences(string text)
-{
-    char temp;
-    int sentences = 0;
-
-    //Iterate through each character in the sentence
-    for (int c = 0; c < strlen(text); c++)
-    {
-        temp = text[c]; // Set the current character to a temp variable
-
-        // Check if current character is either an explanation mark, aperiod or a question mark and add 1 to sentences
-        if (temp == 33 || temp == 46 || temp == 63)
-        {
-            sentences++;
-        }
-    }
-
-    return sentences;
 }
