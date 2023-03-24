dff70c26ef14b8a8b3b390d4e5f28c15a225181c
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-24 20:25:59 +0000

    Fri, Mar 24, 2023, 3:25 PM -05:00

---
+++ Python/dictionary.py
@@ -0,0 +1,12 @@
+pizzas = {
+    "cheese": 9,
+    "pepperoni": 10,
+    "vegetable": 11,
+    "buffalo chicken": 12
+}
+
+pizzas["cheese"] = 8
+print(pizzas["cheese"])
+
+pizzas["bacon"] = 14
+print(pizzas["bacon"])
