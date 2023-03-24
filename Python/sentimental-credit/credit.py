cbbc64313a0c3635c0832a315c116b9f7ae55a01
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-24 00:34:13 +0000

    Thu, Mar 23, 2023, 7:34 PM -05:00

--- Python/sentimental-credit/credit.py
+++ Python/sentimental-credit/credit.py
@@ -32,14 +32,17 @@ for c in reversed(range(0,clength)):  # run loop in reverse to go from back of c
 locator = clength - 2
 
 for a in range(0, clength, 2):  # run loop in reverse skipping every other number
-    currentNo = ccno[locator] * 2  # double the current number in the array
+    if (locator > 0):
+        currentNo = ccno[locator] * 2  # double the current number in the array
+    else:
+        currentNo = 0
 
     while (currentNo > 9):  # check if current number as more than 1 digit
         product = int(currentNo % 10) + int(currentNo / 10)  # if so then add them together
         currentNo = product  # set new number to current number
     checksum += currentNo
     checksum += int(ccno[locator + 1])
-    print("Position ", locator, " : ", currentNo, ", Position ", locator+1, ": ", ccno[locator + 1], ". Checksum:", checksum)
+    #print("Position ", locator, " : ", currentNo, ", Position ", locator+1, ": ", ccno[locator + 1], ". Checksum:", checksum)
     locator -= 2
 
 #checksum = sum(twotimed) + sum(regular)  # add current number to checksum
