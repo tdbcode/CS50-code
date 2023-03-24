4c4d668b0f91138c62e8a60fbff25f087f5b3956
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-24 17:12:20 +0000

    Fri, Mar 24, 2023, 12:12 PM -05:00

--- Python/dna/dna.py
+++ Python/dna/dna.py
@@ -34,7 +34,8 @@ def main():
     print(sequences)
     print(dlength)
     for row in database:  # Format: name,AGATC,AATG,TATC
-        sequences[0] = row[keys[]]  # replace string with int for each team's rating in dictionary
+        for i in range (len(key):
+            sequences[i] = row[keys[i]]  # replace string with int for each team's rating in dictionary
         AATG = row['AATG']
         TATC = row['TATC']
         print(sequences[0]) # for testing
