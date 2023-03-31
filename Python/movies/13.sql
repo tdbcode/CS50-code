34c9895f6096f706f90f8edd23a49ff382b6f6a8
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-31 21:36:52 +0000

    Fri, Mar 31, 2023, 4:36 PM -05:00

--- Python/movies/13.sql
+++ Python/movies/13.sql
@@ -1,9 +1,10 @@
 select distinct(name) from people
 JOIN stars ON stars.person_id = people.id
-JOIN stars.movie_id = movies.id
+JOIN movies ON stars.movie_id = movies.id
 WHERE movies.id IN
 (
-    select id FROM movies
+    select movies.id FROM movies
     JOIN stars ON movie.id = stars.movie_id
     JOIN people ON stars.person_id = people.id
-    WHERE people.name = "Kevin Bacon" AND people.birth = 1958);
+    WHERE people.name = "Kevin Bacon" AND people.birth = 1958
+);
