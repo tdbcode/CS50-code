73dd9afd9021cb06b171aa95bfb76d0ab379ebc1
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-31 16:21:22 +0000

    Fri, Mar 31, 2023, 11:21 AM -05:00

--- Python/prophecy/sql.py
+++ Python/prophecy/sql.py
@@ -14,34 +14,24 @@ with open("students.csv", "r") as file:
 
     # Counts
     houses = {}
+    hid = 1
 
 
     # Iterate over CSV file, counting favorites
     for student in students:
-        id = student["id"]
+        sid = student["id"]
         name = student["student_name"]
         house = student["house"]
         head = student["head"]
 
         if house not in houses:
-            houses[house] = head
+            houses[id] = hid
+            houses[name] = house
+            houses[head] = head
+            hid += 1
 
-        if house == "Slytherin":
-            houseID = 1
-        elif house == "Ravenclaw":
-            houseID = 2
-        elif house == "Hufflepuff":
-            houseID = 3
-        elif house == "Gryffindor":
-            houseID = 4
+    print(houses[name])
 
-    print(houses)
 
-    for house in houses:
-        db.execute("INSERT INTO houses (id, house_name, houseID) VALUES (id, name, );)
-id INTEGER,
-    house_name TEXT,
-    head TEXT,
-    PRIMARY KEY(id)
 
        # db.execute("INSERT INTO student (id, student_name, houseID) VALUES (id, name, );)
