5cf4fc0b9cbe7aee158963e4a95a1ad54bfda901
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-31 20:18:33 +0000

    Fri, Mar 31, 2023, 3:18 PM -05:00

--- Python/movies/9.sql
+++ Python/movies/9.sql
@@ -1,1 +1,1 @@
-select name from people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id where movies.title = "Toy Story";
+select name from people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id where movies.year = 2004 ORDER BY people.birth DESC;
