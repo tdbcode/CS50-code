597b00f6b1439333b6b032f3f39b575c56bf515d
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-31 20:34:25 +0000

    Fri, Mar 31, 2023, 3:34 PM -05:00

--- Python/movies/11.sql
+++ Python/movies/11.sql
@@ -1,1 +1,1 @@
-select title from movies JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id JOIN ratings ON movies.id = ratings.movie_id where people.name = "Chadwick Boseman" ORDER BY ratings.rating DESC;
+select title from movies JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id JOIN ratings ON movies.id = ratings.movie_id where people.name = "Chadwick Boseman" ORDER BY ratings.rating DESC LIMIT 5;
