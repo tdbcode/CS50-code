f38ea8789338d40b65ef22973cc55f7d84619f81
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-31 20:05:25 +0000

    Fri, Mar 31, 2023, 3:05 PM -05:00

--- Python/movies/7.sql
+++ Python/movies/7.sql
@@ -1,1 +1,3 @@
-select title, rating from movies ratings where movie_id IN (select id from movies where movies.year = 2010);
+select title, ratings.rating from movies JOIN movies ON movies.id = ratings.movie_id where movies.year = 2010) ORDER BY ratings.rating DESC;
+
+SELECT student_name, houses.house_name FROM houseref JOIN studentsnew ON houseref.studentid = studentsnew.id JOIN houses ON houseref.houseid = houses.id;
