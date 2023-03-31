7ac642c007dbd6930e6f5377d139aa9f55356209
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-31 16:49:15 +0000

    Fri, Mar 31, 2023, 11:49 AM -05:00

--- Python/prophecy/sql.py
+++ Python/prophecy/sql.py
@@ -30,6 +30,9 @@ with open("students.csv", "r") as file:
         sid = student["id"]
         name = student["student_name"]
 
-        db.execute("INSERT INTO studentsnew (id, student_name, head) VALUES (?, ?, ?);", hid, house, head)
+        hid = db.execute("SELECT id FROM houses WHERE ?;", house)
+        print(hid)
+
+        #db.execute("INSERT INTO studentsnew (id, student_name, head) VALUES (?, ?, ?);", hid, house, head)
 
     print(houses)
