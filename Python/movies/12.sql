aa455a742b311d1e18692b04c7f555cc35e9c9e2
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-31 20:43:28 +0000

    Fri, Mar 31, 2023, 3:43 PM -05:00

--- Python/movies/12.sql
+++ Python/movies/12.sql
@@ -1,1 +1,1 @@
-select title from movies JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id where people.name = "Johnny Depp";
+select title FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id where people.name = "Johnny Depp";
