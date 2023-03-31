9a746a7b205906af6ee3a037169073402c163dcc
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-31 20:10:15 +0000

    Fri, Mar 31, 2023, 3:10 PM -05:00

--- Python/movies/7.sql
+++ Python/movies/7.sql
@@ -1,1 +1,1 @@
-select movies.title, ratings.rating from movies JOIN movies ON movies.id = ratings.movie_id where movies.year = 2010 ORDER BY ratings.rating DESC, movies.title DESC;
+select title, ratings.rating from movies JOIN ratings ON movies.id = ratings.movie_id where year = 2010 ORDER BY ratings.rating DESC, movies.title DESC;
