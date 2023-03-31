3870639860227dfffde2c9893bf90a759541602e
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-31 14:22:17 +0000

    Fri, Mar 31, 2023, 9:22 AM -05:00

---
+++ Python/favorites/favorites.sql
@@ -0,0 +1,16 @@
+UPDATE shows SET title = "How I Met Your Mother" WHERE title = "How i met your mother";
+UPDATE shows SET title = "Arrow" WHERE title = "arrow";
+UPDATE shows SET title = "Avatar: The Last Airbender" WHERE title = "avatar: the last airbender";
+UPDATE shows SET title = "Brooklyn Nine-Nine" WHERE title = "booklyn nine-nine";
+UPDATE shows SET title = "Game of Thrones" WHERE title = "GoT";
+UPDATE shows SET title = "Brooklyn Nine-Nine" WHERE title = "brooklyn 99";
+UPDATE shows SET title = "Community" WHERE title = "community";
+UPDATE shows SET title = "Family Guy" WHERE title = "family guy";
+UPDATE shows SET title = "Friends" WHERE title = "friends";
+UPDATE shows SET title = "Sherlock Holmes" WHERE title = "sherlock holmes";
+UPDATE shows SET title = "Squid Game" WHERE title = "squid game";
+UPDATE shows SET title = "The Untamed" WHERE title = "the Untamed";
+UPDATE shows SET title = "The Bachelorette" WHERE title = "the bachelorette";
+UPDATE shows SET title = "The Office" WHERE title = "the office";
+
+SELECT * FROM shows ORDER BY title;
