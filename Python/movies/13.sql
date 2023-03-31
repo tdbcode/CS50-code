4eb1c82df0315fba58d1830fbf25099176c4a398
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-03-31 21:37:45 +0000

    Fri, Mar 31, 2023, 4:37 PM -05:00

--- Python/movies/13.sql
+++ Python/movies/13.sql
@@ -4,7 +4,7 @@ JOIN movies ON stars.movie_id = movies.id
 WHERE movies.id IN
 (
     select movies.id FROM movies
-    JOIN stars ON movie.id = stars.movie_id
+    JOIN stars ON movies.id = stars.movie_id
     JOIN people ON stars.person_id = people.id
     WHERE people.name = "Kevin Bacon" AND people.birth = 1958
 );
