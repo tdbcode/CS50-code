0d76b1fa60a201074c33a523fb960f3fb354d13e
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-04-03 19:05:23 +0000

    Mon, Apr 3, 2023, 2:05 PM -05:00

--- Python/fiftyville/log.sql
+++ Python/fiftyville/log.sql
@@ -4,7 +4,7 @@ select description from crime_scene_reports where month = 7 and day = 28 and str
 -- Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery. Littering took place at 16:36. No known witnesses.
 
 -- Find interviews on or after the date of 28th July
-select name, day, month, transcript from interviews where month >= 7;
+select name, day, month, transcript from interviews where month >= 7 and day >= 28;
 
 -- Ruth saw the thief get into a car in bakery parking lot within 10 minutes of theft and drove away. Security footage may have cars leaving the parking lot within that time frame
 -- Eugene saw the thief at an ATM on Leggett Street withdrawing money
