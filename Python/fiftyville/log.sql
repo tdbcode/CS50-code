c67b1a8dfef9cf7171f741d55ecfa636f1cc38e3
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-04-03 18:56:50 +0000

    Mon, Apr 3, 2023, 1:56 PM -05:00

--- Python/fiftyville/log.sql
+++ Python/fiftyville/log.sql
@@ -1,2 +1,7 @@
--- Keep a log of any SQL queries you execute as you solve the mystery.
+-- First query, as suggested in video, to find crime scene description
+select description from crime_scene_reports where month = 7 and day = 28 and street = 'Humphrey Street';
+
+-- Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery. Littering took place at 16:36. No known witnesses.
+
+
 
