3cefe895b5b6aaa96ba7bf997f5b3db4d96373ea
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-05-04 15:08:16 +0000

    Thu, May 4, 2023, 10:08 AM -05:00

--- HTML/Flask/Labs/frostimsql/app.py
+++ HTML/Flask/Labs/frostimsql/app.py
@@ -1,10 +1,11 @@
-# Implements a registration form, storing registrants in a dictionary, with error messages
+# Implements a registration form, storing registrants in a SQLite database, with support for deregistration
 
+from cs50 import SQL
 from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
-REGISTRANTS = {}
+db = SQL("sqlite:///froshims.db")
 
 SPORTS = [
     "Basketball",
@@ -18,23 +19,27 @@ def index():
     return render_template("index.html", sports=SPORTS)
 
 
+@app.route("/deregister", methods=["POST"])
+def deregister():
+
+    # Forget registrant
+    id = request.form.get("id")
+    if id:
+        db.execute("DELETE FROM registrants WHERE id = ?", id)
+    return redirect("/registrants")
+
+
 @app.route("/register", methods=["POST"])
 def register():
 
-    # Validate name
+    # Validate submission
     name = request.form.get("name")
-    if not name:
-        return render_template("error.html", message="Missing name")
-
-    # Validate sport
     sport = request.form.get("sport")
-    if not sport:
-        return render_template("error.html", message="Missing sport")
-    if sport not in SPORTS:
-        return render_template("error.html", message="Invalid sport")
+    if not name or sport not in SPORTS:
+        return render_template("failure.html")
 
     # Remember registrant
-    REGISTRANTS[name] = sport
+    db.execute("INSERT INTO registrants (name, sport) VALUES(?, ?)", name, sport)
 
     # Confirm registration
     return redirect("/registrants")
@@ -42,4 +47,5 @@ def register():
 
 @app.route("/registrants")
 def registrants():
-    return render_template("registrants.html", registrants=REGISTRANTS)
+    registrants = db.execute("SELECT * FROM registrants")
+    return render_template("registrants.html", registrants=registrants)
