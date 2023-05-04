da4f460488545dfbefabbd380f71f26abb27f276
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-05-04 14:57:21 +0000

    Thu, May 4, 2023, 9:57 AM -05:00

--- HTML/Flask/Labs/PostFlask/app.py
+++ HTML/Flask/Labs/PostFlask/app.py
@@ -1,15 +1,12 @@
-# Switches to POST
+# Uses a single route
 
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        return render_template("greet.html", name=request.form.get("name", "world"))
     return render_template("index.html")
-
-
-@app.route("/greet", methods=["POST"])
-def greet():
-    return render_template("greet.html", name=request.form.get("name", "world"))
