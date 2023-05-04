22d4362a796c5cb3cb0bf7012b6471b90ca248f3
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-05-04 16:00:04 +0000

    Thu, May 4, 2023, 11:00 AM -05:00

--- HTML/Flask/birthdays/app.py
+++ HTML/Flask/birthdays/app.py
@@ -26,7 +26,13 @@ def after_request(response):
 def index():
     if request.method == "POST":
 
-        # TODO: Add the user's entry into the database
+        # Validate submission
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
+
+        # Remember registrant
+        db.execute("INSERT INTO birthdays (name, month day) VALUES(?, ?)", name, month, day)
 
         return redirect("/")
 
