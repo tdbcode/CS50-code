0ecb4794dfda67f1e9d6136d478728d5cd324e7c
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-05-15 20:04:37 +0000

    Mon, May 15, 2023, 3:04 PM -05:00

--- HTML/Flask/finance/app.py
+++ HTML/Flask/finance/app.py
@@ -87,10 +87,11 @@ def changepass():
         # Get users current has from database
         dbhash = db.execute("SELECT hash FROM users WHERE id = ?", session["user_id"])
         hash = dbhash[0]["hash"]
+        chash = generate_password_hash(cpassword, method='pbkdf2:sha256', salt_length=8)
+        print(chash)
         print(hash)
-        print(cpassword)
 
-        if not hash == generate_password_hash(cpassword):
+        if not hash == generate_password_hash(cpassword, method='pbkdf2:sha256', salt_length=8):
             return apology("Password entered does not match current password")
 
         # Ensure current password was submitted
