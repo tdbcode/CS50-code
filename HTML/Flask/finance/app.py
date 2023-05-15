25b0162a8b1cbf34645ca31a13127ed615173602
Thomas Brady <45001007+thomasbrady87@users.noreply.github.com>
2023-05-15 15:46:55 +0000

    Mon, May 15, 2023, 10:46 AM -05:00

--- HTML/Flask/finance/app.py
+++ HTML/Flask/finance/app.py
@@ -275,6 +275,7 @@ def sell():
         except:
             return apology("Invalid shares quantity entered")
         results = lookup(symbol) # Lookup symbol using function which returns list of stock details
+
         print(results)
 
         # If no results then symbol wrong or empty - tell user to enter a valid symbol
